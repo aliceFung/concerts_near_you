bf14b9f2f2c9f1278328b5f3feb8bb32022d7bfc
ayva <olga.ayva@gmail.com>
2015-08-14 15:52:03 -0700

    multiple events on map

--- app/models/bands.rb
+++ app/models/bands.rb
@@ -4,34 +4,43 @@ class Bands
   # base_uri "http://api.bandsintown.com/artists"
   # Bandsintown.app_id = 'Practice_with_API'
 
-  attr_accessor :name, :info
+  attr_accessor :name, :location
 
   def initialize(name, location)
+    @name = name
+    @location = location
     #@options = { :query => { :name => name} }
     #For artist by name search 
     #response_row = HTTParty.get("http://api.bandsintown.com/artists/#{name}.json?api_version=2.0&app_id=practice")
 
     response_row = HTTParty.get("http://api.bandsintown.com/artists/#{name}/events/search.json?api_version=2.0&app_id=YOUR_APP_ID&location=#{location}&radius=10")
     #Make a hash from string
-    @response = JSON.parse(response_row.response.body)
+    @response_json = JSON.parse(response_row.response.body)
 
   end
 
+  def all
+    arr = []
+    @response_json.size.times do |i|
+      arr << @response_json[i]
+    end
+    arr
+  end
 
   def artist
-    @response[0]["artists"].first["name"]
+    self["artists"][0]["name"]
   end
 
   def thumb
-    @response[0]["artists"].first["thumb_url"]
+    self["artists"][0]["thumb_url"]
   end
 
   def title
-    @response[0]["title"]
+    self["title"]
   end
 
   def datetime
-    @response[0]["formatted_datetime"]
+    self["formatted_datetime"]
   end
 
   def description
