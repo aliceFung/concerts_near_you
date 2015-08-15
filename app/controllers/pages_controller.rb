4d1b3d44a2e90a40cbc23507f448e04fe8fb9054
ayva <olga.ayva@gmail.com>
2015-08-14 22:53:57 -0700

    fix seed file and empty params on the main

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,15 @@ class PagesController < ApplicationController
  
   def index
     if current_user
+      #Need to be refactored into Event model
       @events = MapList.user_interest_events
     else
-      @origin = params[:address]
+      #Alice please redo to default location or location by IP if no params
+      params[:address].nil? ? @origin = 'New York' : @origin = params[:address]
       @origin ||= address_from_ip
-      #First option 
-      @events = Bands.new(params[:artist], @origin).events
+      #First option
+      params[:artist].nil? ? artist = 'Madonna' : artist = params[:artist]
+      @events = Bands.new(artist, @origin).events
       #Second option @events = Last.new('New York').events
 
       @hash = Gmaps4rails.build_markers(@events) do |event, marker|
